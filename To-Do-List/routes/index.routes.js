56f02264498837016c0211079d185a3e3b90e7a9
hvkst <bernd@hvkst.de>
2022-10-13 15:44:51 +0200

    small things, take away one 'save changes'-button, logged in users go to dashboard, others go to home.

--- To-Do-List/routes/index.routes.js
+++ To-Do-List/routes/index.routes.js
@@ -2,11 +2,8 @@ const router = require('express').Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  // res.render("index");
-  // res.render('auth/login');
-  console.log(req.session);
-  if (req.session) {
-    return res.redirect("/dashboard");
+  if (req.isAuthenticated()) {
+    return res.redirect('/dashboard');
   }
   res.render('index');
 });
