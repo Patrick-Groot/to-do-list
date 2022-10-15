0509b7c8b99b5aa6db5040af5329bbe07790aefb
hvkst <bernd@hvkst.de>
2022-10-15 10:22:16 +0200

    next

--- public/js/script.js
+++ public/js/script.js
@@ -6,16 +6,16 @@ document.addEventListener(
   false
 );
 
-const expandButton = document.getElementById('expandButton');
-const doneList = document.getElementById('done');
+// const expandButton = document.getElementById('expandButton');
+// const doneList = document.getElementById('done');
 
-expandButton.addEventListener('click', () => {
-  const expandButtonAria = expandButton.getAttribute('aria-expanded');
-  if (expandButtonAria == 'true') {
-    expandButton.innerHTML = 'hide done &uarr;';
-    doneList.style.backgroundColor = 'rgb(225, 255, 228)';
-  } else {
-    expandButton.innerHTML = 'show done &darr;';
-    doneList.style.backgroundColor = 'transparent';
-  }
-});
+// expandButton.addEventListener('click', () => {
+//   const expandButtonAria = expandButton.getAttribute('aria-expanded');
+//   if (expandButtonAria == 'true') {
+//     expandButton.innerHTML = 'hide done &uarr;';
+//     doneList.style.backgroundColor = 'rgb(225, 255, 228)';
+//   } else {
+//     expandButton.innerHTML = 'show done &darr;';
+//     doneList.style.backgroundColor = 'transparent';
+//   }
+// });
