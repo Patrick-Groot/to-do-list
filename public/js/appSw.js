dbb551df7c2bfcb41ad9a17f910cc27f175c96bd
hvkst <bernd@hvkst.de>
2022-10-15 20:00:31 +0200

    webMainfest updating

--- public/js/appSw.js
+++ public/js/appSw.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('../../sw.js')
+    .register('/sw.js')
     .then((reg) => console.log('service worker registered.', reg))
     .catch((err) => console.log('service worker NOT registered.', err));
 }
