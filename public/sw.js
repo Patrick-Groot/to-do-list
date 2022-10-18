6534bf43778dea90f4fa583ba42640dade753e42
hvkst <bernd@hvkst.de>
2022-10-18 19:15:38 +0200

    Layout almost done...

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,14 @@
 // install service worker
 self.addEventListener('install', (evt) => {
-  console.log('service worker has been installed');
+  // console.log('service worker has been installed');
 });
 
 // activate event
 self.addEventListener('activate', (evt) => {
-  console.log('service worker has been activated');
+  // console.log('service worker has been activated');
 });
 
 // fetch events
 self.addEventListener('fetch', (evt) => {
-  console.log('fetch event', evt);
+  // console.log('fetch event', evt);
 });
